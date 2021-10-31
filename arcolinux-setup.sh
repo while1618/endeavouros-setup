ece0ee80db9e3b5af3ad22ad983cc9fac30fc6fa
Dejan Zdravkovic <bagzi1996@gmail.com>
2021-10-31 14:00:38 +0100

    arcolinux script update

--- arcolinux-setup.sh
+++ arcolinux-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # Things to pick during installation:
 # Kernel:
@@ -14,7 +14,7 @@
 # ArcoLinuxTools:
 #	- fun
 #	- sddm-themes
-#	- steam
+#	- steam(if gaming)
 #	- utilities
 # Communication:
 #	- discord
@@ -44,7 +44,7 @@
 #	- mailspring
 # Theming:
 #	- whatever suits you(currently all)
-# Gaming:
+# Gaming:(optional)
 #	- meta-steam
 #	- wine
 #	- lutris
@@ -54,7 +54,6 @@
 #	- alacritty
 #	- oh-my-zsh
 #	- powerlevel10k
-#	- terminator
 # Filemanagers:
 #	- dolphin
 #	- ranger
@@ -65,7 +64,6 @@
 #	- snapd
 #	- htop
 #	- lm_sensors
-#	- caffeine
 #	- galculator
 #	- gufw
 #	- stacer
@@ -81,12 +79,14 @@ echo "Before running check the following link"
 echo "https://arcolinux.com/things-to-do-after-arcolinux-installation/"
 echo "While the script is running, you should customize your theme and change default apps if needed."
 
-read -r -p "Have you check installation script before running? [Y/n] " response
-if [[ "$response" =~ ^([nN][eE][sS]|[nN])$ ]]
+read -r -p "Have you checked the installation script before running? [Y/n] " check
+if [[ "$check" =~ ^([nN][eE][sS]|[nN])$ ]]
 then
     echo "Please check it."
     exit
 fi
+
+read -r -p "Are you on NVIDIA gpu? [y/N]" nvidia
 
 # load custom arcolinux scripts
 skel
@@ -119,12 +119,12 @@ sudo systemctl enable --now mariadb
 sudo mysql_secure_installation
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 
-# postgres (set password to "root")
+# postgres
 sudo pacman -S postgresql
 sudo su - postgres -c "initdb -D '/var/lib/postgres/data'"
 sudo systemctl enable postgresql
 sudo systemctl start postgresql
-sudo psql -U postgres -c "\password postgres"
+sudo psql -U postgres -c "ALTER USER postgres PASSWORD 'root';"
 
 # redis
 sudo pacman -S redis
@@ -156,12 +156,17 @@ yay postman-bin
 yay intellij-idea-ultimate-edition
 yay pycharm-professional
 
-# hardware
-# gpu config
-sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
-yay gwe
-# rgb config (r: 200, g: 140: b:255)
-yay openrgb
+if [[ "$nvidia" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+    # hardware
+    # gpu config
+    sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
+    yay gwe
+    # rgb config (r: 200, g: 140: b:255)
+    yay openrgb
+fi
+
+echo "Done!"
 
 # reboot if needed
 read -r -p "Do you want to reboot now? [y/N] " response

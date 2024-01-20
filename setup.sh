98a2643b5745b36a59c32b14600e084ebf889669
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-01-20 16:49:18 +0100

    update

--- setup.sh
+++ setup.sh
@@ -169,6 +169,7 @@ sudo pacman -Sy neovim --noconfirm
 yay vim-plug
 sudo pacman -Sy fzf --noconfirm
 sudo pacman -Sy ripgrep --noconfirm
+sudo pacman -Sy fd --noconfirm
 sudo pacman -Sy nnn --noconfirm
 sudo pacman -Sy qalculate-gtk --noconfirm
 sudo pacman -Sy gparted --noconfirm

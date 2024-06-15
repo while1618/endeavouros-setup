8aa27bc6d14772d7b89c265f1e6bb1084addcbe8
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-06-15 13:35:49 +0200

    remove bspwm

--- setup.sh
+++ setup.sh
@@ -1,283 +1,360 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$(id -u)" = 0 ]; then
-    echo "##################################################################"
-    echo "This script MUST NOT be run as root user since it makes changes"
-    echo "to the \$HOME directory of the \$USER executing this script."
-    echo "The \$HOME directory of the root user is, of course, '/root'."
-    echo "We don't want to mess around in there. So run this script as a"
-    echo "normal user. You will be asked for a sudo password when necessary."
-    echo "##################################################################"
+    echo ":: This script shouldn't be run as root."
     exit 1
 fi
 
-echo "###################################################"
-echo "## Before running check the installation script. ##"
-echo "###################################################"
+clear
+GREEN='\033[0;32m'
+NONE='\033[0m'
 
-read -r -p "Have you checked? [Y/n] " check
-if [[ "$check" =~ ^([nN][eE][sS]|[nN])$ ]]; then
-    echo "######################"
-    echo "## Please check it. ##"
-    echo "######################"
-    exit 1
+# ----------------------------------------------------- 
+# functions
+# ----------------------------------------------------- 
+
+# check if package is installed
+_isInstalledPacman() {
+    package="$1";
+    check="$(sudo pacman -Qs --color always "${package}" | grep "local" | grep "${package} ")";
+    if [ -n "${check}" ] ; then
+        echo 0; #'0' means 'true' in Bash
+        return; #true
+    fi;
+    echo 1; #'1' means 'false' in Bash
+    return; #false
+}
+
+# install required packages
+_installPackagesPacman() {
+    toInstall=();
+    for pkg; do
+        if [[ $(_isInstalledPacman "${pkg}") == 0 ]]; then
+            echo "${pkg} is already installed.";
+            continue;
+        fi;
+        toInstall+=("${pkg}");
+    done;
+    if [[ "${toInstall[@]}" == "" ]] ; then
+        # echo "All pacman packages are already installed.";
+        return;
+    fi;
+    printf "Package not installed:\n%s\n" "${toInstall[@]}";
+    sudo pacman --noconfirm -S "${toInstall[@]}";
+}
+
+# required packages for the installer
+installer_packages=(
+    "wget"
+    "unzip"
+    "gum"
+    "figlet"
+)
+
+if gum confirm "You should check the installation script before you continue? Have you?" ;then
+    echo
+    echo ":: Installing Hyprland and additional packages"
+    echo
+elif [ $? -eq 130 ]; then
+    exit 130
+else
+    echo
+    echo ":: Installation canceled."
+    exit;
 fi
-read -r -p "On which platform are you runinng? [laptop/pc] " platform
-read -r -p "Do you want to enable bluetooth? [y/N] " bluetooth
-read -r -p "Enter the size of the swap file (e.g. 8 for 8gb): " swap
-read -r -p "Enter git name: " git_name
-read -r -p "Enter git email: " git_email
 
-echo "####################"
-echo "## Update system. ##"
-echo "####################"
-sudo pacman -Syyu --noconfirm
-yay -Su --noconfirm
+echo -e "${GREEN}"
+cat <<"EOF"
+ _   _                  _                 _ 
+| | | |_   _ _ __  _ __| | __ _ _ __   __| |
+| |_| | | | | '_ \| '__| |/ _` | '_ \ / _` |
+|  _  | |_| | |_) | |  | | (_| | | | | (_| |
+|_| |_|\__, | .__/|_|  |_|\__,_|_| |_|\__,_|
+       |___/|_|                             
 
-echo "######################"
-echo "## Enable firewall. ##"
-echo "######################"
-sudo pacman -Sy ufw --noconfirm
-sudo ufw enable
+EOF
+echo -e "${NONE}"
 
-echo "###############"
-echo "## SSD Trim. ##"
-echo "###############"
-sudo systemctl enable fstrim.timer
+echo "What is the resolution and refresh rate of your monitor?"
+echo "Answare in the following format eg. 3440x1440@144"
+resolution=$(gum input --placeholder "Resolution and refresh rate...")
+echo "Resolution and refresh rate: ${resolution}"
 
-echo "###########"
-echo "## Swap. ##"
-echo "###########"
-echo "vm.swappiness=10" | sudo tee -a /etc/sysctl.d/100-arcolinux.conf
-sudo swapoff -a
-sudo dd if=/dev/zero of=/swapfile bs=1G count=$swap
-sudo mkswap /swapfile
-sudo swapon /swapfile
+if gum confirm "Are you using Nvidia GPU?" ;then
+    nvidia=true
+    echo
+    echo ":: Nvidia GPU is not officially supported by Hyprland. If you face any problems, please check Hyprland Wiki"
+    echo ":: https://wiki.hyprland.org/Nvidia/"
+    echo
+    if gum confirm "Continue?" ;then
+        echo
+        echo ":: Installing Nvidia GPU Drivers"
+        echo
+        sudo pacman -Sy nvidia-inst --noconfirm
+    elif [ $? -eq 130 ]; then
+        exit 130
+    else
+        echo
+        echo ":: Installation canceled."
+        exit;
+    fi
+fi
 
-echo "#################"
-echo "## Git config. ##"
-echo "#################"
+# ----------------------------------------------------- 
+# synchronizing package databases
+# ----------------------------------------------------- 
+sudo pacman -Sy
+echo
+
+# ----------------------------------------------------- 
+# install required packages
+# ----------------------------------------------------- 
+echo ":: Checking that required packages are installed..."
+_installPackagesPacman "${installer_packages[@]}";
+echo
+
+
+# ----------------------------------------------------- 
+# core packages
+# ----------------------------------------------------- 
+
+echo -e "${GREEN}"
+figlet "CorePackages"
+echo -e "${NONE}"
+
+# packages
+sudo pacman -Sy hyprland waybar rofi-wayland dunst hyprpaper hyprlock hypridle xdg-desktop-portal-hyprland sddm \
+                alacritty vim zsh picom qt5-wayland qt6-wayland cliphist thunar \
+                network-manager-applet blueman brightnessctl \
+                ttf-font-awesome otf-font-awesome ttf-fira-sans ttf-fira-code   \
+                ttf-firacode-nerd papirus-icon-theme gnome-themes-extra gtk-engine-murrine nwg-look \
+                --noconfirm
+yay -S wlogout waypaper bibata-cursor-theme orchis-theme --noconfirm
+
+# hyprland config app
+bash <(curl -s "https://gitlab.com/stephan-raabe/ml4w-hyprland-settings/-/raw/main/setup.sh")
+
+# oh my zsh
+sh -c "$(wget https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+
+
+# -----------------------------------------------------
+# development   
+# ----------------------------------------------------- 
+
+# git
+echo -e "${GREEN}"
+figlet "Git"
+echo -e "${NONE}"
+git_name=$(gum input --placeholder "Enter git name...")
+echo "Name: ${git_name}"
+git_email=$(gum input --placeholder "Enter git email...")
+echo "Email: ${git_email}"
 git config --global user.name "${git_name}"
 git config --global user.email "${git_email}"
 git config --global pull.ff only
-
-echo "########################"
-echo "## Generate ssh keys. ##"
-echo "########################"
 ssh-keygen
 
-echo "#########################################################"
-echo "## Install mysql - current password for root is empty. ##"
-echo "#########################################################"
+# java
+echo -e "${GREEN}"
+figlet "Java"
+echo -e "${NONE}"
+sudo pacman -Sy jre21-openjdk jdk21-openjdk maven --noconfirm
+yay -S google-java-format intellij-idea-ultimate-edition --noconfirm
+
+# python
+echo -e "${GREEN}"
+figlet "Python"
+echo -e "${NONE}"
+sudo pacman -Sy python-pip --noconfirm
+yay -S pycharm-professional --noconfirm
+
+# haskell
+# echo -e "${GREEN}"
+# figlet "Haskell"
+# echo -e "${NONE}"
+# curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
+# yay -S hlint-bin --noconfirm
+
+# mysql
+echo -e "${GREEN}"
+figlet "Mysql"
+echo -e "${NONE}"
 sudo pacman -Sy mariadb --noconfirm
 sudo mysql_install_db --user=mysql --basedir=/usr --datadir=/var/lib/mysql
 sudo systemctl enable --now mariadb
 sudo mysql_secure_installation
 sudo mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';"
 
-echo "#########################"
-echo "## Install postgresql. ##"
-echo "#########################"
+# postgres
+echo -e "${GREEN}"
+figlet "Postgres"
+echo -e "${NONE}"
 sudo pacman -Sy postgresql --noconfirm
 sudo su - postgres -c "initdb -D '/var/lib/postgres/data'"
-sudo systemctl enable postgresql
-sudo systemctl start postgresql
+sudo systemctl enable --now postgresql
 sudo psql -U postgres -c "ALTER USER postgres PASSWORD 'root';"
 
-echo "####################"
-echo "## Install redis. ##"
-echo "####################"
+# redis
+echo -e "${GREEN}"
+figlet "Redis"
+echo -e "${NONE}"
 sudo pacman -Sy redis --noconfirm
-sudo systemctl enable redis
-sudo systemctl start redis
+sudo systemctl enable --now redis
 redis-cli config set requirepass root
 
-echo "#####################"
-echo "## Install docker. ##"
-echo "#####################"
-sudo pacman -Sy docker --noconfirm
-sudo systemctl start docker.service
-sudo systemctl enable docker.service
-sudo usermod -aG docker $USER
-sudo pacman -Sy docker-compose --noconfirm
-
-echo "#################################################"
-echo "## Install java, maven and google-java-format. ##"
-echo "#################################################"
-sudo pacman -Sy jre11-openjdk --noconfirm
-sudo pacman -Sy jdk11-openjdk --noconfirm
-sudo pacman -Sy jre17-openjdk --noconfirm
-sudo pacman -Sy jdk17-openjdk --noconfirm
-sudo pacman -Sy jre21-openjdk --noconfirm
-sudo pacman -Sy jdk21-openjdk --noconfirm
-sudo pacman -Sy maven --noconfirm
-yay google-java-format
-
-echo "###########################"
-echo "## Install node via nvm. ##"
-echo "###########################"
-yay nvm
+# node
+echo -e "${GREEN}"
+figlet "Node"
+echo -e "${NONE}"
+yay -S nvm --noconfirm
 source /usr/share/nvm/init-nvm.sh
 nvm install --lts
 
-echo "##################"
-echo "## Install pip. ##"
-echo "##################"
-sudo pacman -Sy python-pip --noconfirm
+# docker
+echo -e "${GREEN}"
+figlet "Docker"
+echo -e "${NONE}"
+sudo pacman -Sy docker --noconfirm
+sudo systemctl enable --now docker.service
+sudo usermod -aG docker $USER
+sudo pacman -Sy docker-compose --noconfirm
 
-echo "######################"
-echo "## Install Haskell. ##"
-echo "######################"
-curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
-yay -S hlint-bin
+# vscode
+echo -e "${GREEN}"
+figlet "VSCode"
+echo -e "${NONE}"
+sudo pacman -Sy gnome-keyring --noconfirm
+yay -S vscode insomnia-bin --noconfirm
 
-echo "##################"
-echo "## Install zsh. ##"
-echo "##################"
-sudo pacman -Sy zsh --noconfirm
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
-
-echo "###################################################"
-echo "## Fix for not starting java based apps in bspwm ##"
-echo "###################################################"
-echo "export _JAVA_AWT_WM_NONREPARENTING=1" | sudo tee -a /etc/profile
-
-echo "###################"
-echo "## Install apps. ##"
-echo "###################"
-sudo pacman -Sy signal-desktop --noconfirm
-sudo pacman -Sy discord --noconfirm
-yay onlyoffice
-sudo pacman -Sy okular --noconfirm
-sudo pacman -Sy feh --noconfirm
-sudo pacman -Sy gwenview --noconfirm
-sudo pacman -Sy vlc --noconfirm
-yay brave-bin
-sudo pacman -Sy qbittorrent --noconfirm
-sudo pacman -Sy bitwarden --noconfirm
-sudo pacman -Sy alacritty --noconfirm
-yay vscode
-sudo pacman -Sy gnome-keyring
-yay insomnia-bin
-yay intellij-idea-ultimate-edition
-yay pycharm-professional
-sudo pacman -Sy vim --noconfirm
-sudo pacman -Sy neovim --noconfirm
-yay vim-plug
-sudo pacman -Sy fzf --noconfirm
-sudo pacman -Sy ripgrep --noconfirm
-sudo pacman -Sy fd --noconfirm
-sudo pacman -Sy nnn --noconfirm
-sudo pacman -Sy qalculate-gtk --noconfirm
-sudo pacman -Sy gparted --noconfirm
-sudo pacman -Sy veracrypt --noconfirm
-yay ventoy-bin
-yay unified-remote-server
-sudo pacman -Sy picom --noconfirm
-
-echo "#################"
-echo "## Add themes. ##"
-echo "#################"
-mkdir -p ~/.themes && tar xf themes/Nordic-darker.tar.xz -C ~/.themes/
-mkdir -p ~/.icons && tar xf icons/Nordic-Folders.tar.xz -C ~/.icons/
-tar xf icons/volantes_light_cursors.tar.gz -C ~/.icons/
-cp -rf config/theme/.gtkrc-2.0 ~/
-cp -rf config/theme/gtk-3.0/settings.ini ~/.config/gtk-3.0/
-cp config/theme/.Xresources ~/
-xrdb ~/.Xresources
-cp config/theme/qbittorrent.qbtheme ~/.config/qBittorrent
-
-echo "################"
-echo "## Add fonts. ##"
-echo "################"
-mkdir -p ~/.fonts && cp -rf fonts/** ~/.fonts/
-sudo pacman -Sy ttf-fira-code
-
-echo "######################"
-echo "## Add backgrounds. ##"
-echo "######################"
-sudo mkdir -p /usr/share/endeavouros/backgrounds/while1618_wallpapers/
-sudo cp backgrounds/** /usr/share/endeavouros/backgrounds/while1618_wallpapers/
-nitrogen
-
-echo "#######################"
-echo "## Add config files. ##"
-echo "#######################"
-mkdir -p ~/.config/alacritty/ && cp -rf config/alacritty/** ~/.config/alacritty/
-cp -rf config/bspwm/** ~/.config/bspwm/
-cp -rf config/polybar/** ~/.config/polybar/
-cp -rf config/rofi/** ~/.config/rofi/
-cp -rf config/sxhkd/** ~/.config/sxhkd/
-rm -rf ~/.mozilla/firefox/*.default-release/**
-cp config/firefox/prefs.js ~/.mozilla/firefox/*.default-release/
-cp -rf config/shell/.bashrc ~/
-cp -rf config/shell/.zshrc ~/
-cp -rf config/shell/.nanorc ~/
-cp -rf config/shell/.p10k.zsh ~/
-git clone https://github.com/NvChad/NvChad ~/.config/nvchad --depth
-git clone --depth 1 https://github.com/AstroNvim/AstroNvim ~/.config/astrovim
+# neovim
+echo -e "${GREEN}"
+figlet "Neovim"
+echo -e "${NONE}"
+sudo pacman -Sy neovim fzf ripgrep fd --noconfirm
+yay -S vim-plug --noconfirm
+git clone https://github.com/NvChad/starter ~/.config/nvchad
+git clone --depth 1 https://github.com/AstroNvim/template ~/.config/astronvim
 git clone https://github.com/LazyVim/starter ~/.config/lazyvim
 
-if [[ "$platform" == "laptop" ]]; then
-    echo "#############"
-    echo "## Laptop. ##"
-    echo "#############"
-    sed -i "62s/.*/modules-right = keyboard sps cpu sps memory sps pulseaudio sps brightness sps battery sps updates sps date/" ~/.config/polybar/config.ini
-    sed -i "227s/.*/label-maxlen = 75/" ~/.config/polybar/config.ini
-    sed -i "187s/.*/  size: 7.0/" ~/.config/alacritty/alacritty.yml
-    echo "xinput --set-prop 'SYNA2B2C:01 06CB:7F27 Touchpad' 'libinput Natural Scrolling Enabled' 1 &" | tee -a ~/.config/bspwm/bspwmrc
-    echo "HandleLidSwitch=ignore" | sudo tee -a /etc/systemd/logind.conf
-elif [[ "$platform" == "pc" ]]; then
-    echo "#########"
-    echo "## PC. ##"
-    echo "#########"
-    sudo nvidia-xconfig
-    sudo nvidia-xconfig -a --cool-bits=28 --allow-empty-initial-configuration
-    sudo pacman -Sy psensor --noconfirm
-    yay cpu-x
-    sudo pacman -Sy piper --noconfirm
-    yay openrgb     # rgb config -> mb: (r: 200, g: 140: b:255), gpu: (r: 100, g: 50, b: 100)
-    sudo sensors-detect
-    echo "xrandr --output DP-4 --mode 3440x1440 --rate 144.00 &" | tee -a ~/.config/bspwm/bspwmrc
-    # sudo pacman -Sy numlockx --noconfirm
-    # echo "numlockx on &" | tee -a ~/.config/bspwm/bspwmrc
-    # yay gwe         # gpu fan config -> (50-0, 54-8, 58-18, 60-60, 65-80, 70-100)
-    # echo "gwe --hide-window &" | tee -a ~/.config/bspwm/bspwmrc
-else
-    echo "#######################"
-    echo "## Unknown platform. ##"
-    echo "#######################"
+
+# -----------------------------------------------------
+# apps
+# ----------------------------------------------------- 
+
+# gui
+echo -e "${GREEN}"
+figlet "GUI"
+echo -e "${NONE}"
+sudo pacman -Sy discord okular feh gwenview vlc qbittorrent bitwarden qalculate-gtk gparted veracrypt --noconfirm
+yay -S onlyoffice brave-bin ventoy-bin unified-remote-server --noconfirm
+
+# terminal utils
+echo -e "${GREEN}"
+figlet "TerminalUtils"
+echo -e "${NONE}"
+sudo pacman -Sy tmux nnn fastfetch --noconfirm
+
+
+# -----------------------------------------------------
+# configs and themes
+# ----------------------------------------------------- 
+
+# dotfiles
+echo -e "${GREEN}"
+figlet "Dotfiles"
+echo -e "${NONE}"
+
+if $nvidia ;then
+    echo \
+"# ----------------------------------------------------- 
+# Environment Variables
+# ----------------------------------------------------- 
+
+https://wiki.hyprland.org/Nvidia/
+env = LIBVA_DRIVER_NAME,nvidia
+env = XDG_SESSION_TYPE,wayland
+env = GBM_BACKEND,nvidia-drm
+env = __GLX_VENDOR_LIBRARY_NAME,nvidia" > ./config/hypr/conf/environment.conf
 fi
 
-if [[ "$bluetooth" =~ ^([yN][eE][sS]|[yN])$ ]]; then
-    echo "#######################"
-    echo "## Enable bluetooth. ##"
-    echo "#######################"
-    sudo pacman -Sy --needed bluez bluez-utils --noconfirm
-    sudo systemctl enable --now bluetooth
-    sudo pacman -Sy blueberry --noconfirm
-    echo "blueberry-tray &" | tee -a ~/.config/bspwm/bspwmrc
-fi
+echo \
+"# ----------------------------------------------------- 
+# Monitor Setup
+# ----------------------------------------------------- 
 
-echo "##################################"
-echo "## Change default shell to zsh. ##"
-echo "##################################"
-chsh -s $(which zsh)
+monitor=,${resolution},auto,1" > ./config/hypr/conf/monitor.conf
 
-echo "#####################"
-echo "## System cleanup. ##"
-echo "#####################"
+cp -rf ./config/.gtkrc-2.0 ./config/.Xresources ./config/.bashrc ./config/.zshrc ./config/.p10k.zsh ~/
+mkdir -p ~/.config/qBittorrent && cp -rf ./config/qbittorrent/qbittorrent.qbtheme ~/.config/qBittorrent
+cp -rf ./config/alacritty ./config/dunst ./config/gtk-3.0 ./config/gtk-4.0 ./config/hypr ./config/picom ./config/scripts ./config/wal ./config/waybar ./config/wlogout ~/.config
+
+# rofi
+echo -e "${GREEN}"
+figlet "Rofi"
+echo -e "${NONE}"
+git clone --depth=1 https://github.com/adi1090x/rofi.git ~/rofi
+cd ~/rofi
+chmod +x setup.sh
+sh setup.sh
+cd -
+rm -rf ~/rofi
+
+# sddm
+echo -e "${GREEN}"
+figlet "SDDM"
+echo -e "${NONE}"
+sudo systemctl enable sddm
+sudo git clone https://github.com/keyitdev/sddm-astronaut-theme.git /usr/share/sddm/themes/sddm-astronaut-theme
+sudo cp /usr/share/sddm/themes/sddm-astronaut-theme/Fonts/* /usr/share/fonts/
+echo "[Theme]
+Current=sddm-astronaut-theme" | sudo tee /etc/sddm.conf
+
+# wallpapers
+echo -e "${GREEN}"
+figlet "Wallpapers"
+echo -e "${NONE}"
+cp -r wallpapers/** ~/Pictures
+
+# system configs
+echo -e "${GREEN}"
+figlet "SystemConfigs"
+echo -e "${NONE}"
+sudo systemctl enable bluetooth
+sudo systemctl enable fstrim.timer
+
+# swapfile
+echo -e "${GREEN}"
+figlet "Swapfile"
+echo -e "${NONE}"
+sudo mkswap -U clear --size 8G --file /swapfile
+sudo swapon /swapfile
+echo "/swapfile				  swap		 swap	 defaults   0 0" | sudo tee -a /etc/fstab
+
+
+# -----------------------------------------------------
+# kernel
+# -----------------------------------------------------
+
+# zen kernel
+echo -e "${GREEN}"
+figlet "ZenKernel"
+echo -e "${NONE}"
+sudo pacman -Sy linux-zen linux-zen-headers --noconfirm
+sudo grub-mkconfig -o /boot/grub/grub.cfg
+
+# cleanup
+echo -e "${GREEN}"
+figlet "Cleanup"
+echo -e "${NONE}"
 sudo pacman -Rns $(pacman -Qtdq) --noconfirm
 yay -Sc --noconfirm
 
-echo "###########"
-echo "## Done! ##"
-echo "###########"
+echo -e "${GREEN}"
+figlet "Done"
+echo -e "${NONE}"
 
-read -r -p "Do you want to reboot now? [y/N] " response
-if [[ "$response" =~ ^([yY][eE][sS]|[yY])$ ]]; then
-    reboot
-fi
+echo
+echo "DONE! Please reboot your system!"

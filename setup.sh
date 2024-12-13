f36f55c5682317dc824599b48fb904b3fdea3436
Dejan Zdravkovic <dejan.zdravkovic@protonmail.com>
2024-12-13 03:40:04 +0100

    nvidia noconfirm

--- setup.sh
+++ setup.sh
@@ -370,7 +370,7 @@ if $nvidia ;then
     echo -e "${GREEN}"
     figlet "Nvidia"
     echo -e "${NONE}"
-    sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings libva-nvidia-driver
+    sudo pacman -S nvidia-dkms nvidia-utils lib32-nvidia-utils nvidia-settings libva-nvidia-driver --noconfirm
     echo "force_drivers+=\" nvidia nvidia_modeset nvidia_uvm nvidia_drm \"" | sudo tee -a /etc/dracut.conf.d/nvidia.conf
     echo "options nvidia_drm modeset=1 fbdev=1" | sudo tee -a /etc/modprobe.d/nvidia.conf
     if [[ "$boot" == "systemd" ]]; then
